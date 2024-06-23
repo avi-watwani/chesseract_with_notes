14da8ed511ec6ee6652fcb3d7788dc70905bc159
avi-watwani <avi.watwani@gmail.com>
2024-06-23 21:39:51 +0530

    added NewNote form

---
+++ app/javascript/components/NewNote.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const NewNote = () => {
+  const navigate = useNavigate();
+  const [content, setContent] = useState("");
+
+  const stripHtmlEntities = (str) => {
+    return String(str)
+      .replace(/\n/g, "<br> <br>")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+  };
+
+  const onChange = (event, setFunction) => {
+    setFunction(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const url = "/api/v1/notes/create";
+
+    if (content.length == 0)
+      return;
+
+    const body = {
+      content: stripHtmlEntities(content),
+    };
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => navigate(`/note/${response.id}`))
+      .catch((error) => console.log(error.message));
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-sm-12 col-lg-6 offset-lg-3">
+          <h1 className="font-weight-normal mb-5">
+            Add a new note
+          </h1>
+          <form onSubmit={onSubmit}>
+            <label htmlFor="content">Note Content</label>
+            <textarea
+              className="form-control"
+              id="content"
+              name="content"
+              rows="10"
+              onChange={(event) => onChange(event, setContent)}
+            />
+            <button type="submit" className="btn custom-button mt-3">
+              Create Note
+            </button>
+            <Link to="/notes" className="btn btn-link mt-3">
+              Back to notes
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewNote;
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Notes from "../components/Notes";
 import Note from "../components/Note";
+import NewNote from "../components/NewNote";
 
 export default (
   <Router>
@@ -10,6 +11,7 @@ export default (
       <Route path="/" element={<Home />} />
       <Route path="/notes" element={<Notes />} />
       <Route path="/note/:id" element={<Note />} />
+      <Route path="/note" element={<NewNote />} />
     </Routes>
   </Router>
 );
